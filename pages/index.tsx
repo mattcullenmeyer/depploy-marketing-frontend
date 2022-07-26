9ba58fda0559677782ec529a0a270434c93b19ad
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-07-25 20:30:32 -0600

    eslint and prettier added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
-import {Anchor} from '@twilio-paste/core/anchor';
-import {Heading} from '@twilio-paste/core/heading';
-import {Box} from '@twilio-paste/core/box';
-import {Paragraph} from '@twilio-paste/core/paragraph';
-import {ListItem, UnorderedList} from '@twilio-paste/core/list';
-import {Separator} from '@twilio-paste/core/separator';
-import type {NextPage} from 'next';
+import { Anchor } from '@twilio-paste/core/anchor';
+import { Heading } from '@twilio-paste/core/heading';
+import { Box } from '@twilio-paste/core/box';
+import { Paragraph } from '@twilio-paste/core/paragraph';
+import { ListItem, UnorderedList } from '@twilio-paste/core/list';
+import { Separator } from '@twilio-paste/core/separator';
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
@@ -20,8 +20,8 @@ const Home: NextPage = () => {
       </Heading>
 
       <Paragraph>
-        Everything you need to get started using Paste in a Production app. Start by editing{' '}
-        <code>pages/index.tsx</code>
+        Everything you need to get started using Paste in a Production app.
+        Start by editing <code>pages/index.tsx</code>
       </Paragraph>
       <Separator orientation="horizontal" verticalSpacing="space120" />
 
@@ -37,7 +37,8 @@ const Home: NextPage = () => {
             </Anchor>
           </Heading>
           <Paragraph>
-            Start here. Find in-depth information about using the Paste Design System to build your Next app.
+            Start here. Find in-depth information about using the Paste Design
+            System to build your Next app.
           </Paragraph>
         </ListItem>
         <ListItem>
@@ -46,15 +47,22 @@ const Home: NextPage = () => {
               NextJS Documentation
             </Anchor>
           </Heading>
-          <Paragraph>Find in-depth information about Next.js features and API.</Paragraph>
+          <Paragraph>
+            Find in-depth information about Next.js features and API.
+          </Paragraph>
         </ListItem>
         <ListItem>
           <Heading as="h3" variant="heading30">
-            <Anchor href="https://vercel.com/import?filter=next.js" showExternal>
+            <Anchor
+              href="https://vercel.com/import?filter=next.js"
+              showExternal
+            >
               Deploy
             </Anchor>
           </Heading>
-          <Paragraph>Instantly deploy your Next.js site to a public URL with Vercel.</Paragraph>
+          <Paragraph>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </Paragraph>
         </ListItem>
       </UnorderedList>
     </Box>
