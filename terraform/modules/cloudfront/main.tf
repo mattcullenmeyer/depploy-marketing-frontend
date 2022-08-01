fdd69e8784bdcd10dbb7e76c3acaa149d1a89dd3
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-07-31 21:09:40 -0600

    Added managed cache policy

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -1,3 +1,8 @@
+locals {
+  # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
+  managed_caching_optimized_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_origin_access_identity
 resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access_identity" {}
 
@@ -5,7 +10,7 @@ resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access_ident
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
     domain_name = var.s3_bucket_regional_domain_name
-    origin_id = var.s3_bucket_id
+    origin_id   = var.s3_bucket_id
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.cloudfront_access_identity_path
@@ -20,20 +25,12 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = var.s3_bucket_id
-    
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
+    cache_policy_id        = local.managed_caching_optimized_id
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 1
-    default_ttl            = 86400
-    max_ttl                = 31536000
     compress               = true
   }
+
+  price_class = "PriceClass_All"
 
   restrictions {
     geo_restriction {
@@ -42,8 +39,8 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = var.acm_certificate_arn
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
 
