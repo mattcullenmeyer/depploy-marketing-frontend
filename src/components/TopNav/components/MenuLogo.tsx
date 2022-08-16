a5f90ba7b6d6ec0162175e164e2e71697a0e4d6d
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-08-16 10:22:11 -0600

    [depploy-13] Marketing Site: Fix TopNav on Mobile

--- src/components/TopNav/components/MenuLogo.tsx
+++ src/components/TopNav/components/MenuLogo.tsx
@@ -1,5 +1,4 @@
 import { Box } from '@twilio-paste/core/box';
-import { Stack } from '@twilio-paste/core/stack';
 import { Text } from '@twilio-paste/core/text';
 import { GiFairyWings } from 'react-icons/gi';
 import { links } from '../words';
@@ -7,22 +6,24 @@ import variables from '../../../styles/variables.module.scss';
 
 export function MenuLogo() {
   return (
-    <Box as="a" href={links.home} textDecoration="none">
-      <Stack orientation="horizontal" spacing="space40">
-        <Box as="div" display="flex" alignItems="center">
-          <GiFairyWings
-            style={{ fontSize: '40px', color: variables.primaryColor }}
-          />
-        </Box>
-        <Stack orientation="vertical" spacing="space10">
-          <Text as="div" fontSize="fontSize40" fontWeight="fontWeightSemibold">
-            Depploy
-          </Text>
-          <Text as="div" fontSize="fontSize30" fontWeight="fontWeightMedium">
-            Infrastructure as Code
-          </Text>
-        </Stack>
-      </Stack>
+    <Box
+      as="a"
+      href={links.home}
+      textDecoration="none"
+      display="flex"
+      alignItems="center"
+      columnGap="space30"
+    >
+      <Box as="div" display="flex">
+        <GiFairyWings
+          style={{ fontSize: '35px', color: variables.primaryColor }}
+        />
+      </Box>
+      <Box as="div">
+        <Text as="div" fontSize="fontSize60" fontWeight="fontWeightSemibold">
+          Depploy
+        </Text>
+      </Box>
     </Box>
   );
 }
--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -25,22 +25,22 @@ export function TopNav(): React.ReactElement {
       >
         <MenuLogo />
         <Box as="nav">
-          <Stack orientation="horizontal" spacing="space90">
+          <Stack orientation="horizontal" spacing="space80">
             <MenuLink href={links.products} label={words.products} />
             <MenuLink href={links.blog} label={words.blog} />
             <MenuLink href={links.pricing} label={words.pricing} />
           </Stack>
         </Box>
         <Box>
-          <Stack orientation="horizontal" spacing="space70">
+          <Stack orientation="horizontal" spacing="space80">
             <MenuLink href={links.signin} label={words.signin} />
             <Box className={styles.menu_signup_button}>
               <Button variant="primary" onClick={onClickGetStarted}>
                 {words.getStarted}
               </Button>
             </Box>
-            <MobileMenuButton />
           </Stack>
+          <MobileMenuButton />
         </Box>
       </Box>
     </Box>
