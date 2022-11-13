d3056979166221c19c1ca3b1de3e6d09d13c73c6
mattcullenmeyer <mattcullenmeyer@gmail.com>
2022-11-13 12:19:32 -0700

    added stuff

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 // client.js
 import sanityClient from "@sanity/client";
 
-export default sanityClient({
+export const client = sanityClient({
   projectId: "1gqvndqq", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
+  dataset: "production",
   useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: "2021-08-31",
 });
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,42 +1,92 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import groq from 'groq';
 import { PortableText } from '@portabletext/react';
-import client from '../../../client';
+import { client } from '../../../client';
+import styles from './blog.module.scss';
+import { Box } from '@twilio-paste/core/box';
+import { Heading } from '@twilio-paste/core/heading';
+import React from 'react';
+import { Paragraph } from '@twilio-paste/core';
 
 interface PostProps {
   post: {
     title: string;
     body: any[];
+    // headings: any[];
   };
 }
 
-interface TextBlock {
-  value: {
-    children: [{ text: string }];
-  };
-}
+// interface PortableTextBlockComponent {
+//   children: [{ text: string }];
+// }
+
+// interface TextBlock {
+//   value: {
+//     children: [{ text: string }];
+//     style: string;
+//   };
+// }
+
+const generateHref = (heading: string) => {
+  const headingArray = heading.split(' ');
+  const lowerCaseHeadingArray = headingArray.map((text) => text.toLowerCase());
+  const result = lowerCaseHeadingArray.join('-');
+  return result;
+};
 
 const portableTextComponents = {
-  types: {
-    block: ({ value }: TextBlock) => {
-      return <div>{value.children[0].text}</div>;
-    },
+  block: {
+    normal: ({ children }: any) => <Paragraph>{children}</Paragraph>,
+    h2: ({ children }: any) => (
+      <Heading as="h2" variant="heading20">
+        {children}
+      </Heading>
+    ),
   },
 };
 
 function Post({ post }: PostProps) {
+  if (!post) {
+    return;
+  }
+
+  const headingItems = post.body.filter((item) => item.style === 'h2');
+  const headings = headingItems.map(({ children }) => children[0].text);
+
   return (
-    <>
-      <>{post?.title}</>
-      <PortableText value={post?.body} components={portableTextComponents} />
-    </>
+    <Box className={styles.container}>
+      <Box>
+        <Heading as="div" variant="heading40">
+          Contents
+        </Heading>
+        <ul style={{ paddingInlineStart: '0' }}>
+          {headings.map((heading) => (
+            <li>
+              <a href={`#${generateHref(heading)}`}>{heading}</a>
+            </li>
+          ))}
+        </ul>
+      </Box>
+      <Box>
+        <Heading as="h1" variant="heading10">
+          {post.title}
+        </Heading>
+        <PortableText value={post.body} components={portableTextComponents} />
+      </Box>
+      <Box>
+        <Heading as="div" variant="heading40">
+          Popular Posts
+        </Heading>
+      </Box>
+    </Box>
   );
 }
 
 const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
   body,
-  _updatedAt
+  // _updatedAt,
+  // "headings": body[string::startsWith(style, "h")],
 }`;
 
 export const getStaticPaths: GetStaticPaths = async () => {
